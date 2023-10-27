d17ad09f406a9729b615f4e35269fcbace3647e0
ShintaRaudita <shintaraudita2110@gmail.com>
2023-10-27 13:48:45 +0700

    js login register

--- js/register.js
+++ js/register.js
@@ -1,21 +1,21 @@
-// Ambil elemen-elemen HTML yang diperlukan
+// Ambil elemen-elemen HTML yang diperlukan   
 const usernameInput = document.getElementById("username");
 const passwordInput = document.getElementById("password");
 const submitButton = document.getElementById("submit");
 const registerForm = document.getElementById("registerForm");
 
-// URL API register
+// URL API register   
 const registerApiUrl = "https://asia-southeast2-bustling-walker-340203.cloudfunctions.net/function-Signup";
 
 // Tambahkan event listener untuk mengirim permintaan saat formulir dikirim   
 registerForm.addEventListener("submit", async (event) => {
     event.preventDefault(); // Mencegah pengiriman form default
 
-    //Ambil nilai dari input username dan password   
+    //Ambil nilai dari input username dan password
     const username = usernameInput.value;
     const password = passwordInput.value;
 
-    //Kirim permintaan POST ke API register   
+    //Kirim permintaan POST ke API register
     try {
         const response = await fetch(registerApiUrl, {
             method: "POST",
