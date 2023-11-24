169f091c52dadc2cc911168f01010102daa4fc65
AkbarHasballah <salmanakbartafisa@gmail.com>
2023-11-24 15:32:25 +0700

    Add (expression) : Menambahkan kondisi Login admin

--- js/login.js
+++ js/login.js
@@ -1,25 +1,25 @@
-import {setCookieWithExpireHour} from "https://jscroot.github.io/cookie/croot.js";
+import { setCookieWithExpireHour } from "https://jscroot.github.io/cookie/croot.js";
 
 document.addEventListener("DOMContentLoaded", function() {
     const loginForm = document.getElementById("loginForm");
     const message = document.getElementById("message");
     const usernameInput = document.getElementById("username");
-const passwordInput = document.getElementById("password");
+    const passwordInput = document.getElementById("password");
     const submitButton = document.getElementById("submit");
     const errorMessage = document.getElementById("error-message");
 
-    //Fungsi untuk mengecek apakah form telah diisi dengan benar
+    // Fungsi untuk mengecek apakah form telah diisi dengan benar
     const validation = () => {
-    const username = usernameInput.value;
-    const pass = passwordInput.value;
-    if (username !== "" && pass !== "") {
-        submitButton.disabled = false;
-    } else {
-        submitButton.disabled = true;
-    }
+        const username = usernameInput.value;
+        const pass = passwordInput.value;
+        if (username !== "" && pass !== "") {
+            submitButton.disabled = false;
+        } else {
+            submitButton.disabled = true;
+        }
     };
 
-    //Panggil fungsi validation saat input berubah
+    // Panggil fungsi validation saat input berubah
     usernameInput.addEventListener("input", validation);
     passwordInput.addEventListener("input", validation);
 
@@ -27,9 +27,9 @@ const passwordInput = document.getElementById("password");
         event.preventDefault();
         
         const username = usernameInput.value;
-    const password = passwordInput.value;
+        const password = passwordInput.value;
 
-        // Kirim permintaan POST ke API
+        // Kirim permintaan POST ke API login user
         fetch("https://us-central1-bustling-walker-340203.cloudfunctions.net/SIgnin", {
             method: "POST",
             headers: {
@@ -43,23 +43,41 @@ const passwordInput = document.getElementById("password");
         .then(response => response.json())
         .then(data => {
             if (data.status === true) {
-                // Pengolahan respons setelah login berhasil
+                // Pengolahan respons setelah login berhasil untuk user biasa
                 const token = data.token;
-                //const welcomeMessage = data.message;
-                //message.textContent = welcomeMessage;
-                setCookieWithExpireHour("token",token,2);
-                //message.style.color = "green";
-                console.log(token);
-
-                // Redirect ke halaman dashboard setelah login berhasil
+                setCookieWithExpireHour("token", token, 2);
                 window.location.href = "../pages/dashboard.html";
             } else {
-                // Pengolahan respons jika login gagal
-                errorMessage.textContent = "Userr not found"; // pesan kesalahan
+                // Cek login untuk admin jika login user biasa gagal
+                fetch("https://asia-southeast2-bustling-walker-340203.cloudfunctions.net/function-SigninAdmin", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify({
+                        username: username,
+                        password: password
+                    })
+                })
+                .then(response => response.json())
+                .then(adminData => {
+                    if (adminData.status === true) {
+                        // Pengolahan respons setelah login berhasil untuk admin
+                        const adminToken = adminData.token;
+                        setCookieWithExpireHour("adminToken", adminToken, 2);
+                        window.location.href = "../pages/Admindashboard.html";
+                    } else {
+                        // Pesan kesalahan jika kedua login gagal
+                        errorMessage.textContent = "User not found"; // Pesan kesalahan
+                    }
+                })
+                .catch(error => {
+                    console.error("Error:", error);
+                });
             }
         })
         .catch(error => {
-            console.error("Errorr:", error);
+            console.error("Error:", error);
         });
     });
 });
