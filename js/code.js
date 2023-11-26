3dcd52eafc269041f8612c4c68052a0e39ebca6d
ShintaRaudita <shintaraudita2110@gmail.com>
2023-11-26 22:40:50 +0700

    update (js) : update js table

--- js/code.js
+++ js/code.js
@@ -1,54 +1,81 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // ... (existing code) ...
-
-    // Event listener for delete button click
-    document.getElementById('table-body').addEventListener('click', function (event) {
-        if (event.target.classList.contains('is-danger')) {
-            // Show the delete confirmation modal
-            document.getElementById('deleteModal').classList.add('is-active');
-
-            // Event listener for confirm delete button click
-            document.getElementById('confirmDelete').addEventListener('click', function () {
-                // Close the modal
-                document.getElementById('deleteModal').classList.remove('is-active');
-
-                // Get the post ID from the clicked row (adjust this based on your data structure)
-                const postId = event.target.closest('tr').dataset.postId;
-
-                // Call the API to delete the post
-                deletePost(postId);
-            });
-
-            // Event listener for modal close
-            document.querySelector('.jb-modal-close').addEventListener('click', function () {
-                document.getElementById('deleteModal').classList.remove('is-active');
-            });
-        }
-    });
-
-    // Function to delete post by calling the API
-    const deletePost = async (postId) => {
-        try {
-            const response = await fetch('https://us-central1-bustling-walker-340203.cloudfunctions.net/function-9DeleteWisata', {
-                method: 'DELETE',
-                headers: {
-                    'Content-Type': 'application/json',
-                    // Add any additional headers if needed
-                },
-                body: JSON.stringify({
-                    filter: { _id: postId }, // Adjust this based on your data structure
-                }),
-            });
-
-            if (!response.ok) {
-                throw new Error('Error deleting post');
-            }
-
-            // Fetch and fill the table again after deletion
-            const data = await fetchData();
-            fillTable(data);
-        } catch (error) {
-            console.error('Error deleting post:', error);
-        }
+    const fetchData = async () => {
+      // ...
     };
-});
+  
+    const fillTable = (data) => {
+      const tableBody = document.getElementById('table-body');
+  
+      // ...
+  
+      data.data.forEach((item) => {
+        const row = `
+          <!-- ... -->
+          <td class="is-actions-cell">
+            <div class="buttons is-right">
+              <button class="button is-small is-warning" type="button">
+                <span class="icon"><i class="mdi mdi-file-edit"></i></span>
+              </button>
+              <button class="button is-small is-danger jb-modal delete-post" data-target="deleteConfirmationModal" data-post-id="${item._id.$oid}" type="button">
+                <span class="icon"><i class="mdi mdi-trash-can"></i></span>
+              </button>
+            </div>
+          </td>
+          <!-- ... -->
+        `;
+        tableBody.innerHTML += row;
+      });
+  
+      // Set up event listeners for delete buttons
+      const deleteButtons = document.querySelectorAll('.delete-post');
+      deleteButtons.forEach(button => {
+        button.addEventListener('click', () => {
+          const postId = button.getAttribute('data-post-id');
+          showDeleteConfirmationModal(postId);
+        });
+      });
+    };
+  
+    const showDeleteConfirmationModal = (postId) => {
+      const deleteConfirmButton = document.getElementById('deleteConfirmButton');
+      deleteConfirmButton.onclick = () => {
+        // Panggil API untuk menghapus data dengan menggunakan postId
+        // Ganti URL dengan URL API delete yang sesuai
+        const apiUrl = `https://us-central1-bustling-walker-340203.cloudfunctions.net/function-9DeleteWisata`;
+        fetch(apiUrl, {
+          method: 'DELETE',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            filter: {
+              _id: {
+                $oid: postId,
+              },
+            },
+          }),
+        })
+          .then(response => response.json())
+          .then(data => {
+            console.log(data);
+            // Lakukan sesuatu setelah berhasil menghapus, misalnya refresh tabel
+            fetchData().then((data) => fillTable(data));
+          })
+          .catch(error => console.error('Error deleting data:', error));
+  
+        // Tutup modal konfirmasi penghapusan
+        closeDeleteConfirmationModal();
+      };
+  
+      // Tampilkan modal konfirmasi penghapusan
+      document.getElementById('deleteConfirmationModal').classList.add('is-active');
+    };
+  
+    const closeDeleteConfirmationModal = () => {
+      // Tutup modal konfirmasi penghapusan
+      document.getElementById('deleteConfirmationModal').classList.remove('is-active');
+    };
+  
+    fetchData().then((data) => fillTable(data));
+  });
+  
