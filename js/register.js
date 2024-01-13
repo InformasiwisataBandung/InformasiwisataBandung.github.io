a2c63d981053c06e86699b342a39f4b8e0878891
AkbarHasballah <salmanakbartafisa@gmail.com>
2024-01-14 01:06:56 +0700

    Update

--- js/register.js
+++ js/register.js
@@ -1,20 +1,16 @@
-// Ambil elemen-elemen HTML yang diperlukan
-const usernameInput = document.getElementById("username");
-const passwordInput = document.getElementById("password");
-const submitButton = document.getElementById("submit");
-const registerForm = document.getElementById("registerForm");
-const errorMessage = document.getElementById("error-message");
-
-// URL API register
-const registerApiUrl = "https://asia-southeast2-bustling-walker-340203.cloudfunctions.net/function-2Signup";
-
-// Tambahkan event listener untuk mengirim permintaan saat formulir dikirim
 registerForm.addEventListener("submit", async (event) => {
     event.preventDefault(); // Mencegah pengiriman form default
 
-    //Ambil nilai dari input username dan password
+    // Tampilkan loading spinner
+    const loadingSpinner = document.getElementById("loading-spinner");
+    loadingSpinner.classList.remove("hidden"); // Menghapus kelas "hidden"
+
+    // Ambil nilai dari input username dan password
+    const no_whatsapp = no_whatsappInput.value;
     const username = usernameInput.value;
     const password = passwordInput.value;
+    const role = "user";
+
 
     //Kirim permintaan POST ke API register
     try {
@@ -24,20 +20,21 @@ registerForm.addEventListener("submit", async (event) => {
             headers: {
                 "Content-Type": "application/json",
             },
-            body: JSON.stringify({ username, password }),
+            body: JSON.stringify({ username, password, no_whatsapp, role }),
         });
 
         if (response.ok) {
-            // Pendaftaran berhasil, alihkan ke halaman login.html
-            //window.location.href = "../pages/login.html";
+            // Pendaftaran berhasil, alihkan ke halaman suksesDaftar.html
             window.location.href = "../pages/suksesDaftar.html";
         } else {
             // Handle kesalahan jika diperlukan
             const data = await response.json();
-            //console.error("Gagall mendaftar:", data.message);
             errorMessage.textContent = data.message; // Menampilkan pesan kesalahan dari API
         }
     } catch (error) {
-        console.error("Terjadi kesalahann:", error);
+        console.error("Terjadi kesalahan:", error);
+    } finally {
+        // Menyembunyikan loading spinner setelah selesai
+        loadingSpinner.style.display = "none";
     }
 });
