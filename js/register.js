66af9dade1c0b878f444faea33af8f830953a997
AkbarHasballah <salmanakbartafisa@gmail.com>
2024-01-15 15:22:24 +0700

    update

--- js/register.js
+++ js/register.js
@@ -1,16 +1,23 @@
+// Ambil elemen-elemen HTML yang diperlukan
+const no_whatsappInput = document.getElementById("no_whatsapp");
+const usernameInput = document.getElementById("username");
+const passwordInput = document.getElementById("password");
+const submitButton = document.getElementById("submit");
+const registerForm = document.getElementById("registerForm");
+const errorMessage = document.getElementById("error-message");
+
+// URL API register
+const registerApiUrl = "https://asia-southeast2-bustling-walker-340203.cloudfunctions.net/function-1SIGNAUTHWA";
+
+// Tambahkan event listener untuk mengirim permintaan saat formulir dikirim
 registerForm.addEventListener("submit", async (event) => {
     event.preventDefault(); // Mencegah pengiriman form default
 
-    // Tampilkan loading spinner
-    const loadingSpinner = document.getElementById("loading-spinner");
-    loadingSpinner.classList.remove("hidden"); // Menghapus kelas "hidden"
-
-    // Ambil nilai dari input username dan password
+    //Ambil nilai dari input username dan password
     const no_whatsapp = no_whatsappInput.value;
     const username = usernameInput.value;
     const password = passwordInput.value;
     const role = "user";
-
 
     //Kirim permintaan POST ke API register
     try {
@@ -24,17 +31,16 @@ registerForm.addEventListener("submit", async (event) => {
         });
 
         if (response.ok) {
-            // Pendaftaran berhasil, alihkan ke halaman suksesDaftar.html
+            // Pendaftaran berhasil, alihkan ke halaman login.html
+            //window.location.href = "../pages/login.html";
             window.location.href = "../pages/suksesDaftar.html";
         } else {
             // Handle kesalahan jika diperlukan
             const data = await response.json();
+            //console.error("Gagall mendaftar:", data.message);
             errorMessage.textContent = data.message; // Menampilkan pesan kesalahan dari API
         }
     } catch (error) {
         console.error("Terjadi kesalahan:", error);
-    } finally {
-        // Menyembunyikan loading spinner setelah selesai
-        loadingSpinner.style.display = "none";
     }
 });
